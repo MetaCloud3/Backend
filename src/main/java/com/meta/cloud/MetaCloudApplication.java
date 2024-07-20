94b252d6c1fe187c489dee08216e2ed8badd70b3
zestbear <zestbear90@gmail.com>
2024-07-20 20:56:51 +0900

    initial commit

---
+++ src/main/java/com/meta/cloud/MetaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.meta.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meta/cloud/MetaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
