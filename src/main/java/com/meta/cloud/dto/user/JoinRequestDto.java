95fe24b0acd3205d5feb9e37408aa09d59d5608c
zestbear <zestbear90@gmail.com>
2024-08-03 23:09:37 +0900

    refactor: Entity 변환 로직 비즈니스 로직에서 제거

--- src/main/java/com/meta/cloud/dto/user/JoinRequestDto.java
+++ src/main/java/com/meta/cloud/dto/user/JoinRequestDto.java
@@ -1,12 +1,15 @@
-package com.meta.cloud.dto.auth;
+package com.meta.cloud.dto.user;
 
+import com.meta.cloud.domain.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 
-@Data
-public class JoinRequest {
+@Getter
+@Builder
+public class JoinRequestDto {
 
     @NotBlank(message = "아이디를 입력해주세요.")
     @Size(min = 5, max = 15, message = "아이디는 5 ~ 15자 이내이어야 합니다")
@@ -23,4 +26,13 @@ public class JoinRequest {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    public User toEntity(String encodedPw) {
+        return User.builder()
+                .loginId(loginId)
+                .loginPw(encodedPw)
+                .name(name)
+                .email(email)
+                .build();
+    }
 }
