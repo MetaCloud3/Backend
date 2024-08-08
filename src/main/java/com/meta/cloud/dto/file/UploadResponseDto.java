7932825716985b536d77ce871c06373911f5986e
Jung-kr <jungwy98@gmail.com>
2024-08-08 15:35:31 +0900

    feature: UploadResponseDto에 다수의 필드 추가

---
+++ src/main/java/com/meta/cloud/dto/file/UploadResponseDto.java
@@ -0,0 +1,41 @@
+package com.meta.cloud.dto.file;
+
+import com.meta.cloud.domain.File;
+import com.meta.cloud.domain.FileType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadResponseDto {
+    private String id;
+    private String uploadFileName;
+    private String storeFileName;
+    private FileType type;
+    private Long size;
+    private String path;
+    private Integer downloaded;
+    private LocalDateTime uploadedAt;
+
+    public UploadResponseDto toDto(File entity) {
+        return UploadResponseDto.builder()
+                .id(entity.getId())
+                .uploadFileName(entity.getUploadFileName())
+                .storeFileName(entity.getStoreFileName())
+                .type(entity.getType())
+                .size(entity.getSize())
+                .path(entity.getPath())
+                .downloaded(entity.getDownloaded())
+                .uploadedAt(entity.getUploadedAt())
+                .build();
+    }
+}
